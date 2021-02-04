abaa93b63753ae26dbc681376fc24317ad4e5a11
Emily Lim <emily.elim04@gmail.com>
2021-02-04 01:37:08 +0000

    refactor to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,14 +9,18 @@ const pool = new Pool({
 
 pool.connect();
 
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2] || 'JUL02';
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id 
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%' 
+WHERE cohorts.name LIKE $1 
 ORDER BY teacher;
-`)
+`
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.teacher}: ${user.cohort}`);
